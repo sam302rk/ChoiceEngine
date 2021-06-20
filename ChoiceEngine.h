805452df015b5751200971abddc451f3bd44f1c6
GamingCrafthd <kvvmischl@gmail.com>
2021-06-20 13:19:55 +0200

    File managment
    Added basic functions for implementation:
    * load_file
    * override_file
    * append_to_file

--- ChoiceEngine.h
+++ ChoiceEngine.h
@@ -4,6 +4,7 @@ namespace choice_engine
 	class implementation
 	{
 		public:
+			// Events
 			void on_new_dialog_element_event(char* author, char* text);	// A new DialogElement got received.
 			void on_voice_line_play_event(char* voice_line);			// A voice-line should be played based on Config-Data.
 			void on_user_input_required_event(char* choices[]);			// Received some new choices for the user to choose from.
@@ -11,6 +12,12 @@ namespace choice_engine
 			void on_chapter_end_event(char* chapter);					// This chapter is complete. This event should be used for achievements based on chapter.
 			void on_loading_start_event();								// Show a loading screen instead of game
 			void on_loading_end_event();								// Loading screen can be hidden, at least the engine is done loading
+			
+			// File Managment
+			char* load_file(char* file_name);							// Load file
+			void override_file(char* file_name, char* content);			// Override file (File's content should become content)
+			void append_to_file(char* file_name, char* content);		// Append to file (File's content should be old content + new content)
+			
 	};
 
 	class engine
