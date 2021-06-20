176cccf0844401fbb9e5dfc45aa3e2a4b607a166
GamingCrafthd <kvvmischl@gmail.com>
2021-06-20 13:14:35 +0200

    Initial Event Calling
    * on_chapter_start_event works
    * on_chapter_end_event works
    * on_loading_start_event works
    * on_loading_end_event works

--- ChoiceEngine.cpp
+++ ChoiceEngine.cpp
@@ -11,17 +11,24 @@ namespace choice_engine
 		
 	public:
 		engine_impl(char* working_dir, char* initial_chapter, implementation* impl) : engine(working_dir, initial_chapter, impl)
-		{ this->impl_ = impl; this->working_dir_ = working_dir; this->chapter_ = initial_chapter; }
+		{
+			this->impl_ = impl; this->working_dir_ = working_dir; this->chapter_ = initial_chapter;
+			get_implementation()->on_chapter_start_event(this->chapter_);
+		}
 		
 		void send_user_input(int number) override
 		{
-			// TODO: Event Calling
+			// TODO: Actually handle input
 		}
 		
 		void load_chapter(char* chapter) override
 		{
+			get_implementation()->on_chapter_end_event(this->chapter_);
+			get_implementation()->on_loading_start_event();
 			this->chapter_ = chapter;
-			// TODO: Event Calling
+			// TODO: Actually loading chapter
+			get_implementation()->on_loading_end_event();
+			get_implementation()->on_chapter_start_event(this->chapter_);
 		}
 
 		implementation* get_implementation() override { return impl_; }
