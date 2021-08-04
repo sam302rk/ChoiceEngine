574489e0c0ce9a0c67f2ac3b0d7945fe9027f7ae
GamingCrafthd <samuel.mischl@gamingcraft.de>
2021-08-04 14:27:19 +0200

    Game: on_event: Add docs & Changed args' type

--- Game.h
+++ Game.h
@@ -6,6 +6,12 @@ namespace choice_engine
 	{
 		public:
 			virtual ~game() = default;
-			virtual void on_event(std::string event_name, std::string args...);
+
+			/// <summary>
+			/// Gets run every time a event gets called by the engine or by event nodes.
+			/// </summary>
+			/// <param name="event_name">Event name with following syntax: namespace::event_name</param>
+			/// <param name="args">Event Arguments, Go to the wiki for more infos.</param>
+			virtual void on_event(std::string* event_name, std::vector<std::string*> args);
 	};
 }
